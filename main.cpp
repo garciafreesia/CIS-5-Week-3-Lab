d8b27b45ba8e137731f0d6af10b4d588c66df0d4
garciafreesia <garciafreesia@gmail.com>
2026-09-13 19:20:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <string>
 
-// Lab 3 — Your Name
+// Lab 3 — Freesia Garcia
 // CIS 5 Week 03 · Types & variables
 
 int main() {
   const int CURRENT_YEAR = 2026;
 
   // TODO: declare and initialize on the same line
-  // std::string name = ...;
-  // int age = ...;
-  // double height_m = ...;
-  // char initial = ...;
-  // bool student = ...;
+  std::string name = "Freesia Garcia";
+  int age = 19;
+  double height_m = 1.67;
+  char initial = 'F';
+  bool student = true;
 
   std::cout << "=== About me ===\n";
   // TODO: labeled prints from the variable names + CURRENT_YEAR
+  std::cout << "Name: " << name << '\n';
+  std::cout << "Height (m): " << height_m << '\n';
+  std::cout << "Initial: " << initial << '\n';
+  std::cout << "Student: " << std::boolalpha << student << '\n';
+  std::cout << "Current Year: " << CURRENT_YEAR << '\n';
 
   return 0;
+}
 }
